6dac159edeba563ec9f0d8fb8c6ba70b88b54e7e
Cash <cash@cashwu.com>
2017-02-10 01:45:49 +0800

    add script api

---
+++ XTeam/Controllers/Api/ScriptApiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using XTeam.Models;
+
+namespace XTeam.Controllers.Api
+{
+    public class ScriptApiController : ApiController
+    {
+        private XTeamEntities db = new XTeamEntities();
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(db.Scripts.ToList());
+        }
+    }
+}
